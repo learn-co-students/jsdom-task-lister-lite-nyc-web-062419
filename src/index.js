28ae71792f6ddb592e7af9712d559af42e328198
Colin <cstatro@gmail.com>
2019-08-06 17:24:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // event listener for submit action
+  const myForm = document.querySelector("form#create-task-form");
+  const removeButton = `<button
+      type="button"
+      onclick="return this.parentNode.remove();"
+      class="remove"
+    >
+      X
+    </button>`;
+
+  const lineItem = toDo => `<li>${toDo}${removeButton}</li>`;
+
+  myForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const myText = document.querySelector("input#new-task-description");
+    const myList = document.querySelector("div#list");
+    const newTask = lineItem(myText.value);
+    // myForm.reset();
+
+    myList.innerHTML += newTask;
+    myText.value = "";
+  });
 });
