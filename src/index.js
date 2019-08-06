307776a29bdabb36956814f40b72d8b93f28c26a
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-06 17:50:20 -0400

    task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("input[type =submit]").addEventListener("click", function (event) {
+    event.preventDefault();
+    let item = document.querySelector("input[type=text]").value
+    let node = document.createElement('LI')
+    let button = document.createElement("button")
+    button.append("X")
+    button.id = item
+    node.append(item, button)
+    node.id = item
+    document.querySelector("#list").appendChild(node)
+
+    // document.querySelector("#list").addEventListener("click", function(event){
+    //   let el = document.querySelector(`li#${event.target.id}`)
+    //   el.remove()
+    // })
+  })
+  document.querySelector("#list").addEventListener("click", function(event){
+    if (event.target.localName === "button"){
+      document.querySelectorAll("li").forEach(function(li){
+        if (li.id === event.target.id){
+          li.remove()
+        }
+      })  
+    }
+    
+  })
 });
