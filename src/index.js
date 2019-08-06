494224bbe4af159858c3c523d20a8aefbffaf783
IanGrubb <yangrubb@gmail.com>
2019-08-06 16:01:30 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("form#create-task-form")
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault()
+    const list = document.querySelector("ul#tasks")
+    let item = document.createElement("li")
+
+    const field = document.querySelector("input#new-task-description")
+    item.innerText = field.value
+    field.value = ""
+    
+    list.append(item)
+  })
 });
