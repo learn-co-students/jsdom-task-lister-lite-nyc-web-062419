89b3bf2f54ab4c0349535df885c81a24083b5445
yoseflehrer <yolehrer@gmail.com>
2019-08-06 17:24:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+  console.log('i have loaded')
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    console.log(event)
+    event.preventDefault();
+    const form = document.querySelector("#create-task-form")
+    const value = document.querySelector("#new-task-description").value
+    const color = document.querySelector("#dropdown").value
+    const span = document.createElement("span")
+    const li = document.createElement("li")
+    li.innerText = value
+    li.style.background = color
+    const ul = document.querySelector("#tasks")
+    span.append(li)
+    ul.append(span)
+    form.reset()
+
+}, false);  
+  
 });
