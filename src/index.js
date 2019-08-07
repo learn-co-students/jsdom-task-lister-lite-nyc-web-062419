65d4fa3e242479d8225502b285d2a59c22ff69e0
KConlon13 <KyleConlon13@gmail.com>
2019-08-06 21:03:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.querySelector("input[type=submit]").addEventListener("click", function(event){
+      event.preventDefault();
+      const wordInput = document.querySelector("input[type=text]")
+      const textNode = document.createTextNode(wordInput.value)
+      const node = document.createElement("LI")
+      node.appendChild(textNode)
+    
+      const newTask = document.querySelector("ul#tasks")
+      document.getElementById("tasks").appendChild(node)
+
+      const xButton = document.createElement("BUTTON")
+      xButton.textContent = "X"
+      node.appendChild(xButton)
+      // delete the node?
+    })
 });
