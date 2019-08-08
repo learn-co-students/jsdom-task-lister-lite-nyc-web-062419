276f3c49c0c7a7cf7cc69ff77c47eac3c64ff592
ghatzid <ghatzid@gmail.com>
2019-08-08 10:09:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("input[type=submit]").addEventListener("click", function(event) {
+    event.preventDefault();
+    let input = document.getElementById("new-task-description") //get task input
+    let textnode = document.createTextNode(input.value);        // Create a text node
+    let node = document.createElement("LI");                    // Create a <li> node
+    node.appendChild(textnode);                                 // Append the text to <li>
+    document.getElementById("tasks").appendChild(node);     // let color = document.getElementById("colors").value
+    input.style.color = 'blue'
+
+
 });
+  
+});
+
